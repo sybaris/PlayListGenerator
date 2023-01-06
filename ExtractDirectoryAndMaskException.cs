a40e9ecd4691f2b987f7e0aea92b5a8e0c01cf66
Sybaris <Sybaris@Club-Internet.Fr>
2023-01-06 09:39:56 +0100

    SonarCloud Code Smell fixes

--- ExtractDirectoryAndMaskException.cs
+++ ExtractDirectoryAndMaskException.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PlayListGenerator
 {
-    internal class ExtractDirectoryAndMaskException
+    public class ExtractDirectoryAndMaskException : Exception
     {
+        public ExtractDirectoryAndMaskException()
+        {
+        }
+
+        public ExtractDirectoryAndMaskException(string? message) : base(message)
+        {
+        }
+
+        public ExtractDirectoryAndMaskException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ExtractDirectoryAndMaskException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
