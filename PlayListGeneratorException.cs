2d5060813542bd57cea10a6d5b082080114e1961
Sybaris <Sybaris@Club-Internet.Fr>
2023-01-06 09:45:23 +0100

    SonarCloud Code Smell fixes

--- PlayListGeneratorException.cs
+++ PlayListGeneratorException.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace PlayListGenerator
 {
+    [Serializable]
     public class PlayListGeneratorException : Exception
     {
         public PlayListGeneratorException()
